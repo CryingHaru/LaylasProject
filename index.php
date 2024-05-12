71198d6303c5e38930a65a70e4fcaac66e379840
HaruCraft <luisaraniva2@gmail.com>
2024-05-12 08:50:02 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>En construccion</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
+</head>
+<body>
+<?php require_once 'src/layout/header.php'; ?>
+<div class="scrollerdiv">
+    <main>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus neque voluptatum assumenda. Enim mollitia obcaecati perspiciatis! Doloribus iste dolores debitis sit minima omnis, odio ut, maxime incidunt nostrum dolorem hic?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi aliquid ducimus delectus reiciendis nobis expedita sint explicabo non fugiat assumenda? Distinctio, quisquam. Similique iste amet eaque voluptate velit odio rem!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae facere doloribus obcaecati libero quos. Laborum maxime ab temporibus, vero suscipit, vitae molestiae, voluptatibus deserunt nihil repudiandae laudantium aspernatur molestias eaque.
+        
+    </main>
+    </div>
+    <?php require_once 'src/layout/footer.php'; ?>
+</body>
+</html>
